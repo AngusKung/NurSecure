ca8092e7895c30b52d416f8fa1b3065d3bdf7d5f
Yi-Lin Juang <frankyjuang@gmail.com>
2019-03-23 16:22:38 +0800

    Add specities

---
+++ NurSecure/specialty.py
@@ -0,0 +1,90 @@
+SPECIALTIES = {
+    'Cardiology': [
+        'blood',
+        'heart',
+    ],
+    'Chest': [
+        'esophagus',
+        'heart',
+        'lung',
+        'lungs',
+        'thoracic',
+        'trachea',
+    ],
+    'Dermatology': [
+        'hair',
+        'mucous',
+        'nail',
+        'nails',
+        'skin',
+    ],
+    'Gastroenterology': [
+        'bile ducts',
+        'colon',
+        'esophagus',
+        'gallbladder',
+        'intestine',
+        'liver',
+        'pancreas',
+        'rectum',
+        'stomach',
+    ],
+    'Metabolism & Endocrinology': [
+        'adrenals',
+        'ovaries',
+        'pancreas',
+        'pituitary',
+        'testes',
+        'thyroid',
+    ],
+    'Nephrology': [
+        'kidney',
+        'kidneys'
+    ],
+    'Neurology': [
+        'bones',
+        'brain',
+        'heart',
+        'kidneys',
+        'lungs',
+        'skin',
+        'spinal',
+    ],
+    'Ophthalmology': [
+        'eye',
+        'eyes',
+    ],
+    'Orthopedics': [
+        'bones',
+        'joints',
+        'ligaments',
+        'muscles',
+        'nerves',
+        'tendons',
+    ],
+    'Otolaryngology': [
+        'ear',
+        'ears',
+        'nose',
+        'throat',
+    ],
+    'Reproductive Endocrinology & Infertility': [
+        'ovaries',
+        'testes'
+    ],
+    'Rheumatology': [
+        'joint',
+        'joints',
+    ],
+    'Urology': [
+        'bladder',
+        'epididymis',
+        'kidneys',
+        'penis',
+        'prostate',
+        'testes',
+        'ureters',
+        'urethra',
+        'vesicles',
+    ],
+}
