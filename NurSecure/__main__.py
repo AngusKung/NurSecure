69e0ae8fb14e7f87ca9d96d6289a70db16ae43f6
Angus Kung <angusthefrog@gmail.com>
2019-03-23 11:42:39 +0800

    Add custom training dialogues

--- NurSecure/__main__.py
+++ NurSecure/__main__.py
@@ -1,7 +1,14 @@
+import logging
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, UbuntuCorpusTrainer
 
 import pdb
+
+LOGFORMAT = "%(asctime).19s %(levelname)s %(filename)s Line %(lineno)s: %(message)s"
+logging.basicConfig(format=LOGFORMAT)
+
+logger = logging.getLogger("fastGraph")
+logger.setLevel(logging.INFO)
 
 class NurSecure:
 	'''
@@ -9,22 +16,50 @@ class NurSecure:
 	'''
 
 	def __init__(self):
-
+		logger.info("Hello world!")
 		self.chatbot = ChatBot('NurSecure')
 
-	def train_english_dialogue(self):
+	def train(self, use_ubuntu_corpus=False):
+		self._train_english_corpus()
+		if use_ubuntu_corpus:
+			self._train_ubuntu_corpus()
+
+		self._train_medical_dialogues()
+
+	def _train_english_corpus(self):
+		logger.info("Training English dialogue...")
 		trainer = ChatterBotCorpusTrainer(self.chatbot)
 		trainer.train("chatterbot.corpus.english")
+
+	def _train_ubuntu_corpus(self):
+		# NOTE: Yet to be fixed.
+		trainer = UbuntuCorpusTrainer(self.chatbot)
+		trainer.train()
+
+	def _train_medical_dialogues(self):
+		logger.info("Training custom medical dialogues")
+		trainer = ChatterBotCorpusTrainer(self.chatbot)
+		trainer.train("data.medical")
 
 	def get_response(self, string):
 		return self.chatbot.get_response(string)
 
 if __name__ == "__main__":
 	nursecure = NurSecure()
-	nursecure.train_english_dialogue()
+	nursecure.train()
 
-	print(nursecure.get_response("How do you do?"))
-	print(nursecure.get_response("Are you a real human?"))
+	test_sentences = [
+		"How do you do?",
+		"Are you a real human?",
+		"I'm not feeling well",
+		"My right knee hurts",
+		"Ouch!",
+		"Should I see a doctor for a thorough check?"
+	]
+
+	for sent in test_sentences:
+		print("Human:\t\t"+sent)
+		print("NurSecure:\t"+str(nursecure.get_response(sent)))
 
 	pdb.set_trace()
 
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ NurSecure is an machine-learning powered chatbot that can analyze descriptions, 
 
 ### Delcarations
 Believe us, we can complete thiw within a weekend.
+
+
+#### Useful Links
+[15 Good Datasets](https://gengo.ai/datasets/15-best-chatbot-datasets-for-machine-learning/)
